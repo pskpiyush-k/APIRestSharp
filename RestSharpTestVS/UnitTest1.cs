c3260506cd30b067e8959a39b3df218b9493b250
piyush <pk.piyush.k95@gmail.com>
2025-03-23 18:05:39 +0530

    Added 2 methods

--- RestSharpTestVS/UnitTest1.cs
+++ RestSharpTestVS/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace RestSharpTestVS
             this.testOutputHelper = testOutputHelper;
         }
         [Fact]
-        public async Task Test1()
+        public async Task GetOperationTest()
         {
             var restClientOptions = new RestClientOptions
             {
@@ -25,15 +25,53 @@ namespace RestSharpTestVS
 
             // Rest Client
             var client = new RestClient(restClientOptions);
-
             // Rest Request
             var request = new RestRequest("Product​/GetProductById​/1");
-
             //Perform operation
             var response = await client.GetAsync<Product>(request);
-
             // Assert
             response?.Name.Should().Be("Keyboard");
+        }
+
+        [Fact]
+        public async Task GetWithQuerySegmentTest()
+        {
+            var restClientOptions = new RestClientOptions
+            {
+                BaseUrl = new Uri("https://localhost:5001/"),
+                RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true
+            };
+
+            // Rest Client
+            var client = new RestClient(restClientOptions);
+            // Rest Request
+            var request = new RestRequest("Product​/GetProductById​/{id}");
+            request.AddUrlSegment("id", 2);
+            //Perform operation
+            var response = await client.GetAsync<Product>(request);
+            // Assert
+            response?.Price.Should().Be(400);
+        }
+
+        [Fact]
+        public async Task GetWithQueryParameterTest()
+        {
+            var restClientOptions = new RestClientOptions
+            {
+                BaseUrl = new Uri("https://localhost:5001/"),
+                RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true
+            };
+
+            // Rest Client
+            var client = new RestClient(restClientOptions);
+            // Rest Request
+            var request = new RestRequest("Product​//Product/GetProductByIdAndName");
+            request.AddQueryParameter("id", 2);
+            request.AddQueryParameter("name", "Monitor");
+            //Perform operation
+            var response = await client.GetAsync<Product>(request);
+            // Assert
+            response?.Price.Should().Be(400);
         }
     }
 }
